c859f1c07d6e0ee8924a173000574ff506cf5ad5
Yuri <glassy4085@gmail.com>
2020-08-02 18:44:54 -0700

    fix section b while loop

--- hw4/src/yj2159_hw4_q4.cpp
+++ hw4/src/yj2159_hw4_q4.cpp
@@ -11,7 +11,7 @@ int main(){
     int num(0);
     int sequence(1);
 
-    cout << "a) Frist read the length of the sequence" << endl;
+    cout << "a) Frist read the length of the sequence." << endl;
     cout << "Please enter the length of the sequence: ";
     cin >> length;
     cout << "Please enter your sequence: " << endl;
@@ -20,13 +20,26 @@ int main(){
         cin >> num;
         sequence *= num;
     } 
+    cout << "The geometric mean is: " << pow(sequence,1.0/(double)length) << endl;
+    cout << endl;
 
-    double n(0);
-    for(double i(0); pow(i,length)<=sequence; i++){
-        n = i;
+    int userInput(0);
+    int total(1);
+    int numCount(0);
+
+    cout << "b) Keep reading the numbers until -1 is entered." << endl;
+    cout << "Please enter a non-empty sequence of positive integers, each one in a separate line.";
+    cout << "End your sequence by typind -1: " << endl;
+
+    while (userInput!=-1) {
+        cin >> userInput;
+        if (userInput!=-1){
+            numCount++;
+            total *= userInput;
+        }
     }
-  
-    cout << "The geometric mean is: " << n << endl;
+   
+    cout << "The geometric mean is: " << pow(total,1.0/(double)numCount) << endl;
 
     return 0;
 }
