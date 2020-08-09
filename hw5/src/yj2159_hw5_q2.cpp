56b18027626ff7f397a577ed17c0ea6befdd28ee
Yuri <glassy4085@gmail.com>
2020-08-09 01:56:01 -0700

    code hw5 q2

--- hw5/src/yj2159_hw5_q2.cpp
+++ hw5/src/yj2159_hw5_q2.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main(){
     using std::cout;
     using std::endl;
     using std::cin;
 
+    srand(time(0));
+
+    int x((rand()%100) + 1);
+    int userGuess(0);
+
+    cout << "I thought of a number between 1 and 100! Try to guess it." << endl;
+    cout << "Range: [1,100], Number of guesses left: 5" << endl;
+    cout << "Your guess: ";
+    cin >> userGuess;
+
+    int rangeMin(1);
+    int rangeMax(100);
+    int guessNumCount(5);
+
+    while (guessNumCount>1) {
+        if (userGuess==x) {
+            cout << "Congrats! You guessed my number in " << guessNumCount << " guesses." << endl;
+            break;
+        } else {
+            if (userGuess<x){
+                cout << "Wrong! My number is bigger!" << endl;
+                cout << endl;
+                rangeMin = userGuess+1;
+                guessNumCount -= 1;
+
+                cout << "Range: [" << rangeMin << "," << rangeMax << "]";
+                cout << "Number of guesses left: " << guessNumCount << endl;
+                cout << "Your guess: ";
+                cin >> userGuess;
+                
+            } else {
+                cout << "Wrong! My number is smaller!" << endl;
+                cout << endl;
+                rangeMax = userGuess-1;
+                guessNumCount -=1;
+
+                cout << "Range: [" << rangeMin << "," << rangeMax << "]" <<endl;
+                cout << "Number of guesses left: " << guessNumCount << endl;
+                cout << "Your guess: ";
+                cin >> userGuess;
+                
+            }
+            
+        }
+    }
+
+    if (guessNumCount==1){
+        cout << "Out of guesses! My number is " << x << endl;
+    }
     
+
+
     return 0;
 }
