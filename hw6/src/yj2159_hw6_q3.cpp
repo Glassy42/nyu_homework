bca4ae1a8f60fc52dede8de65a6e25d36ffa26cc
Yuri <glassy4085@gmail.com>
2020-08-16 03:05:41 -0700

    hw6 logarithm code

---
+++ hw6/src/yj2159_hw6_q3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+double eApprox(int n);
+
+int main() {
+    using std::cout;
+    using std::endl;
+
+    cout.precision(30);
+
+    for (int n(1); n<=15; n++) {
+        cout << "n = " << n << '\t' << eApprox(n) << endl;
+    }
+
+    return 0;
+}
+
+double eApprox(int n) {
+    const int firstNum(1);
+    const double secondNum(1);
+
+    double e(0);
+
+    if (n == 1) {
+        return firstNum;
+    } else if (n == 2) {
+        return firstNum+secondNum;
+    } else {
+        double divNum(0);
+        int fact(1);
+        for (int count(1); count<n; count++){
+            fact *= count;
+            divNum = 1.0/fact;
+            e += divNum;
+        }
+        
+        return e+firstNum;
+    }
+
+}
