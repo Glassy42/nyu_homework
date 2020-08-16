b78caf4e84e6d25839d3ef276dc015bdaa523a24
Yuri <glassy4085@gmail.com>
2020-08-16 04:31:27 -0700

    hw6 divisor coding

---
+++ hw6/src/yj2159_hw6_q4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+
+void printDivisor(int num);
+
+int main() {
+    
+    using std::cin;
+    
+    int userInput(0);
+
+    cout << "Please enter a positive integer >= 2: ";
+    cin >> userInput;
+
+    printDivisor(userInput);
+
+    return 0;
+}
+
+void printDivisor(int num) {
+    for (int numCheck(1); numCheck<sqrt(num); ++numCheck) {
+        if (num%numCheck == 0) {
+            cout << numCheck << " ";
+        }
+    }
+
+    for (int numCheck(sqrt(num)); numCheck>=1; --numCheck) {
+        if (num%numCheck == 0) {
+            cout << num/numCheck << " ";
+        }
+    }
+
+    cout << endl;
+}
