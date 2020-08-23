8f7e56bc3eef1306a713fc998086c96ee17db4c6
Yuri <glassy4085@gmail.com>
2020-08-23 22:11:45 +0900

    add void yearcalender function - but feb is not working

--- hw7/src/test.cpp
+++ hw7/src/test.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
 
+int printMonthCalender(int numOfDays, int startingDay);
+void printYearCalender(int year, int startingDay);
 
 int main() {
     using std::cout;
     using std::endl;
     using std::cin;
 
+    printYearCalender(2016, 1);
 
-    int userInput(0);
+    return 0;
+}
 
-    cout << "enter it: " << endl;
-    cin >> userInput;
+int printMonthCalender(int numOfDays, int startingDay){
+    using std::endl;
+    using std::cout;
 
-    cout << "Mon" << "\t" << "Tue" << "\t" << "Wed" << "\t" << "Thr" << "\t";
-    cout << "Fri" << "\t" << "Sat" << "\t" << "Sun" << endl;
+    int lastDay(0);
 
-    for (int num(1); num<=userInput; num++){
+    for (int num(1); num<=numOfDays; num++) {
         cout << num << "\t";
         if (num%7==0) {
             cout << endl;
         }
     }
+    if (numOfDays%7==0) {
+        lastDay = 7;
+    } else {
+        lastDay = numOfDays%7;
+    }
     cout << endl;
-    return 0;
+
+    return lastDay;
+}
+
+void printYearCalender(int year, int startingDay){
+    
+    std::cout << year << std::endl;
+    int store = printMonthCalender(31,startingDay);
+    std::cout << std::endl;
+    std::cout << store << std::endl;
+
 }
--- hw7/src/yj2159_hw7_q1.cpp
+++ hw7/src/yj2159_hw7_q1.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 
 int printMonthCalender(int numOfDays, int startingDay);
-// void printYearCalender(int year, int startingDay);
+void printYearCalender(int year, int startingDay);
 
 int main() {
     using std::cout;
     using std::endl;
 
-    printMonthCalender(30, 7);
-    printMonthCalender(30, 6);
-    printMonthCalender(30, 5);
-    printMonthCalender(30, 4);
-    printMonthCalender(30, 3);
-    printMonthCalender(30, 2);
-    printMonthCalender(30, 1);
+    printYearCalender(2016, 5);
 
     return 0;
 }
@@ -158,6 +152,50 @@ int printMonthCalender(int numOfDays, int startingDay) {
     return lastDay;
 }
 
-// void printYearCalender(int year, int startingDay) {
+void printYearCalender(int year, int startingDay) {
+    using std::cout;
+    using std::endl;
 
-// }
+        cout << "January " << year << endl;
+        int janLastDay = printMonthCalender(31,startingDay);
+
+    int febLastDay;
+    if (((year%4 == 0)&&(year%100 != 0))||(year%400 == 0)) {
+        cout << "February " << year << endl;
+        febLastDay = printMonthCalender(28, janLastDay+1);
+    } else {
+        cout << "February " << year << endl;
+        febLastDay = printMonthCalender(29, janLastDay+1);
+    }
+
+        cout << "March " << year << endl;
+        int marLastDay = printMonthCalender(31, febLastDay+1);
+
+        cout << "April " << year << endl;
+        int aprLastDay = printMonthCalender(30, marLastDay+1);
+
+        cout << "May " << year << endl;
+        int mayLastDay = printMonthCalender(31, aprLastDay+1);
+
+        cout << "June " << year << endl;
+        int junLastDay = printMonthCalender(30, marLastDay+1);
+
+        cout << "July " << year << endl;
+        int julLastDay = printMonthCalender(31, junLastDay+1);
+
+        cout << "August " << year << endl;
+        int augLastDay = printMonthCalender(31, julLastDay+1);
+
+        cout << "September " << year << endl;
+        int sepLastDay = printMonthCalender(30, augLastDay+1);
+
+        cout << "Octorber " << year << endl;
+        int octLastDay = printMonthCalender(31, sepLastDay+1);
+
+        cout << "November " << year << endl;
+        int novLastDay = printMonthCalender(30, octLastDay+1);
+
+        cout << "December " << year << endl;
+        int decLastDay = printMonthCalender(31, novLastDay+1);
+
+}
