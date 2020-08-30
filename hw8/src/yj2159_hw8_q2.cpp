c3e2eba3f3d6083eb97550e170df72ca652a99eb
Yuri <glassy4085@gmail.com>
2020-08-30 15:01:03 +0900

    write code question2 but it's not working

---
+++ hw8/src/yj2159_hw8_q2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+
+bool isPalindrome(string str);
+
+int main() {
+    using std::cout;
+    using std::endl;
+    using std::cin;
+
+    string str;
+
+    cout << "Please enter a word: ";
+    cin >> str;
+
+    bool result;
+    result = isPalindrome(str);
+
+    if (result) {
+        cout << str << " is a palindrome" << endl;
+    } else { 
+        cout << str << " is not a palindrome" << endl;
+    }
+
+    return 0;
+} 
+
+bool isPalindrome(string str) {
+
+    int strNum = str.length();
+    int num(strNum);
+
+    if (num%2 == 0) { //when the length of the string is even
+        for (int i(0); i<num/2; i++) { 
+            if (str[i] == str[num-1]) {
+            num--;
+            }
+        }
+    } else { //when the length of ths string is odd
+        for (int i(0); i<=num/2; i++) {
+            if (str[i] == str[num-1]) {
+            num--;
+            }
+        }
+    } 
+
+    if (num == strNum/2) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
