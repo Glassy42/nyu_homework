6ae7e10dd19423966c2afcccb01dc6051f73a2d1
Yuri <glassy4085@gmail.com>
2020-08-31 19:13:53 +0900

    write question4, fix question5

--- hw8/src/yj2159_hw8_q4.cpp
+++ hw8/src/yj2159_hw8_q4.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using std::string;
+void matchPin();
 
 int main() {
-    using std::cout;
-    using std::endl;
-    using std::cin;
+
+    matchPin();
 
     return 0;
+}
+
+
+void matchPin() {
+    const int actualPin[] = {1,2,3,4,5};
+    int arrNum[10];
+
+    srand(time(0));
+    for (int i(0); i < 10; ++i) {
+         arrNum[i] = rand() % 3 + 1;
+    }
+
+    std::cout << "Please enter your PIN according to the following mapping: " << std::endl;
+    std::cout << "PIN:\t";
+    for (int i(0); i < 10; ++i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "NUM:\t";
+    for (int i(0); i < 10; ++i) {
+        std::cout << arrNum[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::string userInput;
+    std::cin >> userInput;
+
+    for (int i(0); i < userInput.size(); ++i) {
+        const char num(userInput[i]);
+        const int numAsInt(num-'0');
+
+        const int currentPinDigit(actualPin[i]);
+        if (numAsInt != arrNum[currentPinDigit]) {
+            std::cout << "Your PIN is not correct!" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "Your PIN is correct!" << std::endl;
 }
--- hw8/src/yj2159_hw8_q5.cpp
+++ hw8/src/yj2159_hw8_q5.cpp
@@ -17,15 +17,9 @@ int main() {
     cin >> middleInitial;
     cin >> lastName;
 
-    if (tolower(firstName[0])) {
-        firstName[0] = toupper(firstName[0]);
-    }
-    if (tolower(middleInitial[0])) {
-        middleInitial[0] = toupper(middleInitial[0]);
-    }
-    if (tolower(lastName[0])) {
-        lastName[0] = toupper(lastName[0]);
-    }
+    firstName[0] = toupper(firstName[0]);
+    middleInitial[0] = toupper(middleInitial[0]);
+    lastName[0] = toupper(lastName[0]);
 
     cout << lastName << ", " << firstName << " " << middleInitial.substr(0,1) << "." << endl;
     return 0;
