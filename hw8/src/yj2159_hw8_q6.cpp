2339a5c4aac7f5f1bacf8dd209e2a49250748c5a
Yuri <glassy4085@gmail.com>
2020-08-31 16:04:44 +0900

    write code question6

---
+++ hw8/src/yj2159_hw8_q6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+void findDigit(string str);
+
+int main() {
+    using std::cout;
+    using std::endl;
+    using std::cin;
+    
+
+    string userInput;
+
+    cout << "Please enter a line of text: ";
+    getline(cin, userInput);
+
+    findDigit(userInput);
+
+    return 0;
+}
+
+void findDigit(string str) {
+
+    int length(str.length());
+
+    for (int i(0); i < length; i++) {
+        
+        if (i > 0){
+            if ((isdigit(str[i])) && ((str[i-1] == ' ')||(str[i-1] == 'x'))) {
+                str[i] = 'x';
+            }
+        } else {
+            if ((isdigit(str[0])) && (str[1] == ' ')) {
+                str[0] = 'x';
+            }
+        }
+        
+    }
+
+    std::cout << str << std::endl;
+}
