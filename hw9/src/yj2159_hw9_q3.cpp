82a1ae6e2dbb729ffbd83e4aead40dccba3cd660
Yuri <glassy4085@gmail.com>
2020-09-08 23:37:08 +0900

    write second function - working!

--- hw9/src/yj2159_hw9_q3.cpp
+++ hw9/src/yj2159_hw9_q3.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#include <vector>
-
-using std::vector;
 
 int* getPosNums1 (int* arr, int arrSize, int& outPosArrSize);
-// int* getPosNums2 (int* arr, int arrSize, int* outPosArrSizePtr);
-// void getPosNums3 (int* arr, int arrSize, int*& outPosArr, int& outPosArrSize);
+int* getPosNums2 (int* arr, int arrSize, int* outPosArrSizePtr);
+void getPosNums3 (int* arr, int arrSize, int*& outPosArr, int& outPosArrSize);
 // void getPosNums4 (int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr);
 
 int main(){
@@ -15,6 +12,8 @@ int main(){
 
     int arr[6] = {3, -1, -3, 0, 6, 4};
     int arrSize(6);
+
+//a)---------------------------------------------------------------------
     int outPosArrSize(0);
 
     int* returnArray = getPosNums1(arr, arrSize, outPosArrSize);
@@ -29,6 +28,25 @@ int main(){
     cout << "]" << endl;
 
     delete[] returnArray;
+
+//b)---------------------------------------------------------------------
+
+    int outPosArrSize2(0);
+    int* outPosArrSizePtr = &outPosArrSize2;
+
+    int* returnArray2 = getPosNums2 (arr, arrSize, outPosArrSizePtr);
+
+    cout << "[";
+    for(int i(0); i < *outPosArrSizePtr; ++i) {
+        cout << returnArray2[i];
+        if (i != *outPosArrSizePtr-1) {
+            cout << ",";
+        }
+    }
+    cout << "]" << endl;
+
+    delete[] returnArray2;
+//c)---------------------------------------------------------------------
 
     return 0;
 }
@@ -54,13 +72,37 @@ int* getPosNums1 (int* arr, int arrSize, int& outPosArrSize) {
     return returnArray;
 }
 
-// int* getPosNums2 (int* arr, int arrSize, int* outPosArrSizePtr) {
+int* getPosNums2 (int* arr, int arrSize, int* outPosArrSizePtr) {
 
-// }
+    for (int i(0); i < arrSize; ++i) {
+        if ((arr[i]) > 0) {
+            *outPosArrSizePtr += 1;
+        }
+    }
 
-// void getPosNums3 (int* arr, int arrSize, int*& outPosArr, int& outPosArrSize) {
+    int* returnArray2 = new int[*outPosArrSizePtr];
+    int j(0);
 
-// }
+    for (int i(0); i < arrSize; ++i) {
+        if ((arr[i]) > 0) {
+            returnArray2[j] = arr[i];
+            ++j;
+        }
+    }
+
+    return returnArray2;
+}
+
+void getPosNums3 (int* arr, int arrSize, int*& outPosArr, int& outPosArrSize) {
+    
+    for (int i(0); i < arrSize; ++i) {
+        if (arr[i] > 0) {
+            outPosArrSize++;
+        }
+    }
+
+
+}
 
 // void getPosNums4 (int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr) {
 
