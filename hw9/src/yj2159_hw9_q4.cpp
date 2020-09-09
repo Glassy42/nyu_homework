ffab852caae6d06edb35f6038e9128e66da8e497
Yuri <glassy4085@gmail.com>
2020-09-09 21:43:23 +0900

    write code question4

--- hw9/src/yj2159_hw9_q4.cpp
+++ hw9/src/yj2159_hw9_q4.cpp
@@ -8,12 +8,59 @@ int main(){
     using std::cin;
 
     int arr[6] = {5,2,11,7,6,4};
+    int arrSize(6);
 
-    oddsKeepEvensFlip(arr, 6);
+    oddsKeepEvensFlip(arr, arrSize);
+    
+    cout << "[";
+    for (int i(0); i < arrSize; i++) {
+        cout << arr[i];
+        if (i != arrSize-1) {
+            cout << ",";
+        }
+    }
+    cout << "]" << endl;
 
     return 0;
 }
-void oddsKeepsEvensFlip(int arr[], int arrSize) {
-    //don't forget to delete new array!!!!
-    
+void oddsKeepEvensFlip(int arr[], int arrSize) {
+
+    int oddNum(0);
+    int evenNum(0);
+
+    for (int i(0); i < arrSize; ++i) {
+        if (arr[i]%2 == 0) {
+            evenNum++;
+        } else {
+            oddNum++;
+        }
+    }
+
+    int* oddArray = new int[oddNum];
+    int* evenArray = new int[evenNum];
+    int j(0);
+    int k(0);
+
+    for (int i(0); i < arrSize; ++i) {
+        if (arr[i]%2 != 0) {
+            oddArray[j] = arr[i];
+            j++;
+        } else {
+            evenArray[k] = arr[i];
+            k++;
+        }
+    }
+
+    for (int i(0); i < oddNum; ++i) {
+        arr[i] = oddArray[i];
+    }
+
+    int l(evenNum-1);
+    for (int i(oddNum); i < arrSize; ++i) {
+        arr[i] = evenArray[l];
+        l--;
+    }
+
+    delete [] oddArray;
+    delete [] evenArray;
 }
